c45c69c65b36696ae803696abf594e9cd161d61d
Alfred Borja <aborja@ideyatech.com>
2020-09-11 17:44:38 +0800

    Create reusable views

--- app/src/main/java/com/example/tableregistration/CustomerListActivity.kt
+++ app/src/main/java/com/example/tableregistration/CustomerListActivity.kt
@@ -2,10 +2,18 @@ package com.example.tableregistration
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class CustomerListActivity : AppCompatActivity() {
+
+    private val activityTitle by lazy {
+        findViewById<TextView>(R.id.activity_title)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_customer_list)
+
+        activityTitle.text = getString(R.string.customer_list)
     }
 }
