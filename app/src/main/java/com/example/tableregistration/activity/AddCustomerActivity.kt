250294accdb330065b101ea3ecd89013f1be458c
Alfred Borja <aborja@ideyatech.com>
2020-09-14 10:07:46 +0800

    Create and send notification

--- app/src/main/java/com/example/tableregistration/activity/AddCustomerActivity.kt
+++ app/src/main/java/com/example/tableregistration/activity/AddCustomerActivity.kt
@@ -1,11 +1,19 @@
 package com.example.tableregistration.activity
 
 import android.app.AlertDialog
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.*
 import android.widget.AdapterView.OnItemSelectedListener
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.lifecycle.ViewModelProvider
 import com.example.tableregistration.R
 import com.example.tableregistration.model.TableViewModel
@@ -14,6 +22,8 @@ import com.example.tableregistration.model.TableViewModel
 class AddCustomerActivity : AppCompatActivity() {
 
     private lateinit var tableViewModel: TableViewModel
+    private val CHANNEL_ID = "com.example.tableregistration"
+    private val NOTIFICATION_ID = 12345
 
     private val activityTitle by lazy {
         findViewById<TextView>(R.id.activity_title)
@@ -76,11 +86,51 @@ class AddCustomerActivity : AppCompatActivity() {
                     .setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }.show()
             } else {
                 tableViewModel.saveCustomer(customerName = customerName)
-                Toast.makeText(this, "$customerName has been added",Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "$customerName has been added table",Toast.LENGTH_LONG).show()
+                createNotificationChannel()
+                sendNotification(customerName)
                 finish()
             }
         }
 
         activityTitle.text = getString(R.string.add_customer)
+    }
+
+    private fun sendNotification(customerName: String) {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("New Customer")
+            .setContentText("$customerName has been added to table")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            // Set the intent that will fire when the user taps the notification
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(NOTIFICATION_ID, builder.build())
+        }
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Table Registration"
+            val descriptionText = "Table Registration Notification"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
